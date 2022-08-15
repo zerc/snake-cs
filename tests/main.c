da0e4166e9b1764215936b66e33558a1c896fa99
Vladimir Savin <vladimir@zerc.cc>
2022-08-15 20:03:53 +0100

    Run tests in verbose mode.

--- tests/main.c
+++ tests/main.c
@@ -23,7 +23,7 @@ int main(void)
     s = suite();
     runner = srunner_create(s);
 
-    srunner_run_all(runner, CK_NORMAL);
+    srunner_run_all(runner, CK_VERBOSE);
     no_failed = srunner_ntests_failed(runner);
     srunner_free(runner);
     return (no_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
