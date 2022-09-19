5d4fa0dae715316c151c9c1fb6cac18403e6fee9
Vladimir Savin <vladimir@zerc.cc>
2022-09-19 13:11:30 +0100

    Added curses hello world.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
+#include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    puts("hello");
+    initscr();                 /* Start curses mode 		  */
+    printw("Hello World !!!"); /* Print Hello World		  */
+    refresh();                 /* Print it on to the real screen */
+    getch();                   /* Wait for user input */
+    endwin();                  /* End curses mode		  */
+
     return EXIT_SUCCESS;
 }
